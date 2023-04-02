4860436658914376c2f5ce05c5d57f6e553db4c4
unknown <claitondenardi@gmail.com>
2023-04-02 15:39:14 -0300

    feat: add chakra table and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { Input, Select, Text } from '@chakra-ui/react';
+import { Button, Input, Select, Spinner, Table, TableCaption, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
 import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [file, setFile] = useState(null);
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   async function loadFile(file) {
     await file.text()
       .then((data) => {
+        setLoading(true);
         let arr = data.split("\n");
         setFile(arr);
       })
-      .catch((err) => console.error("Erro ao abrir arquivo", err))
+      .catch((err) => {
+        console.error("Erro ao abrir arquivo", err);
+        setLoading(false);
+      })
   }
 
   useEffect(() => {
@@ -29,6 +34,7 @@ function App() {
         time: arr[1],
         type: arr[3],
         file: arr[4],
+        error: arr[5],
         agent: arr[8],
         url: arr[9],
         status: arr[10],
@@ -38,50 +44,69 @@ function App() {
     });
 
     setData(dataArr);
+    setLoading(false);
   }, [file]);
 
   return (
     <div className="App">
-      <input type="file" onChange={(e) => loadFile(e.target.files[0])} />
+      {/* <label for="file">{file ? "Change log file" : "Select a log file (.txt)"}</label> */}
+      <input type="file" name="file" id="file" onChange={(e) => loadFile(e.target.files[0])}></input>
       <br />
-      {data &&
+      {loading &&
+        <Spinner
+          thickness='4px'
+          speed='0.65s'
+          emptyColor='gray.200'
+          color='blue.500'
+          size='xl'
+        />
+      }
+      {!loading && data &&
         <div>
-          <Text>Date</Text>
-          <Input placeholder='12-12-2012' />
-          <Text>Time</Text>
-          <Input placeholder='16:20' />
-          <Text>Type</Text>
-          <Input placeholder='GET' />
-          <Text>File</Text>
-          <Input placeholder='home.asp' />
-          <Text>Status code</Text>
-          <Select placeholder='Select a status'>
-            <option value='500'>500</option>
-            <option value='302'>302</option>
-            <option value='200'>200</option>
-          </Select>
-          <table>
-            <thead>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Type</th>
-              <th>File/page</th>
-              <th>User agent</th>
-              <th>Status code</th>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr>
-                  <td>{item.date}</td>
-                  <td>{item.time}</td>
-                  <td>{item.type}</td>
-                  <td>{item.file}</td>
-                  <td>{item.agent}</td>
-                  <td>{item.status}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <TableContainer>
+            <Table variant='striped' colorScheme='blue'>
+              <TableCaption>Imperial to metric conversion factors</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Date</Th>
+                  <Th>Time</Th>
+                  <Th>Type</Th>
+                  <Th>File/page</Th>
+                  <Th>Status code</Th>
+                </Tr>
+                <Tr>
+                  <Th><Input variant='outline' focusBorderColor='pink.400' htmlSize={7} placeholder='12/12/2012' /></Th>
+                  <Th><Input htmlSize={5} placeholder='16:20:00' /></Th>
+                  <Th>
+                    <Select placeholder='Select option'>
+                      <option value='GET'>GET</option>
+                      <option value='POST'>POST</option>
+                      <option value='PUT'>PUT</option>
+                    </Select>
+                  </Th>
+                  <Th><Input htmlSize={7} placeholder='home.asp' /></Th>
+                  <Th>
+                    <Select placeholder='Select option'>
+                      <option value='500'>500</option>
+                      <option value='302'>302</option>
+                      <option value='200'>200</option>
+                    </Select>
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.map((item) => (
+                  <Tr>
+                    <Td>{item.date}</Td>
+                    <Td>{item.time}</Td>
+                    <Td>{item.type}</Td>
+                    <Td>{item.file}</Td>
+                    <Td>{item.status}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </div>
       }
     </div>
