894df6ba30870869983f1050946ebb138c9b35de
unknown <claitondenardi@gmail.com>
2023-04-02 14:44:18 -0300

    feat: test chakra-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Input, Select, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [file, setFile] = useState(null)
+  const [file, setFile] = useState(null);
+  const [data, setData] = useState(null);
 
   async function loadFile(file) {
     await file.text()
       .then((data) => {
-        setFile(data);
-        console.log(data)
+        let arr = data.split("\n");
+        setFile(arr);
       })
-      .catch(() => console.error("Erro ao abrir arquivo"))
+      .catch((err) => console.error("Erro ao abrir arquivo", err))
   }
+
+  useEffect(() => {
+    if (!file) return;
+    let dataArr = [];
+
+    file.forEach(element => {
+      let arr = element.split(" ");
+      if (arr[0].includes("#")) return;
+
+      let date = arr[0].split('-').reverse().join('/');
+      let obj = {
+        date: date,
+        time: arr[1],
+        type: arr[3],
+        file: arr[4],
+        agent: arr[8],
+        url: arr[9],
+        status: arr[10],
+      }
+      dataArr.push(obj);
+
+    });
+
+    setData(dataArr);
+  }, [file]);
 
   return (
     <div className="App">
       <input type="file" onChange={(e) => loadFile(e.target.files[0])} />
       <br />
-      <pre>{file}</pre>
+      {data &&
+        <div>
+          <Text>Date</Text>
+          <Input placeholder='12-12-2012' />
+          <Text>Time</Text>
+          <Input placeholder='16:20' />
+          <Text>Type</Text>
+          <Input placeholder='GET' />
+          <Text>File</Text>
+          <Input placeholder='home.asp' />
+          <Text>Status code</Text>
+          <Select placeholder='Select a status'>
+            <option value='500'>500</option>
+            <option value='302'>302</option>
+            <option value='200'>200</option>
+          </Select>
+          <table>
+            <thead>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Type</th>
+              <th>File/page</th>
+              <th>User agent</th>
+              <th>Status code</th>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr>
+                  <td>{item.date}</td>
+                  <td>{item.time}</td>
+                  <td>{item.type}</td>
+                  <td>{item.file}</td>
+                  <td>{item.agent}</td>
+                  <td>{item.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      }
     </div>
   )
 }
